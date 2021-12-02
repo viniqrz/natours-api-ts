2d8dace265eb7f21185c7c03b066985a6c119858
Vinicius Queiroz <viniderp@gmail.com>
2021-12-02 18:49:21 -0300

    ReviewController

---
+++ src/controllers/ReviewController.ts
@@ -0,0 +1,72 @@
+import { ReviewService } from "../services/ReviewService";
+import { Request, Response } from "express";
+
+export class ReviewController {
+  constructor(private reviewService: ReviewService) {
+    this.getAll = this.getAll.bind(this);
+    this.getOne = this.getOne.bind(this);
+    this.update = this.update.bind(this);
+    this.delete = this.delete.bind(this);
+    this.create = this.create.bind(this);
+  }
+
+  public async getAll(req: Request, res: Response): Promise<void> {
+    const reviews = await this.reviewService.getAll();
+
+    res.json({
+      status: "success",
+      data: {
+        reviews,
+      },
+    });
+  }
+
+  public async getOne(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+
+    const review = await this.reviewService.getOne(id);
+
+    res.json({
+      status: "success",
+      data: {
+        review,
+      },
+    });
+  }
+
+  public async create(req: Request, res: Response): Promise<void> {
+    const review = await this.reviewService.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        review,
+      },
+    });
+  }
+
+  public async update(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    const partial = req.body;
+
+    const review = await this.reviewService.update(id, partial);
+
+    res.json({
+      status: "success",
+      data: {
+        review,
+      },
+    });
+  }
+
+  public async delete(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+
+    const review = await this.reviewService.delete(id);
+
+    res.json({
+      status: "success",
+      data: null,
+    });
+  }
+}
